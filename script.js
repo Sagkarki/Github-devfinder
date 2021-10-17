873f32ee41c45afffa5eb1c980f6fa026e097042
Sagun Karki <sagunkarki111@gmail.com>
2021-10-16 23:05:00 -0400

    ADD: Light mode

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const mode = document.getElementById("dark_btn")
 const background = document.body.style
-
+const content= document.getElementsByClassName('content')[0].style
+//const stats = document.getElementsById("statistics").style
+const search = document.getElementById("search_container").style
+const modeText = document.getElementById("modeText")
 
 
 let darkMode = true
@@ -14,6 +17,13 @@ mode.addEventListener("click", function(){
 })
 function dark_mode_properties(){
     background.backgroundColor = "rgb(20,29,47)"
+    backgroundColor = "white"
+    content.backgroundColor= "#808080"
+ //  stats.backgroundColor = "rgb(20,29,47)"
+    search.backgroundColor="#808080"
+    darkMode = true
+    modeText.innerText= "LIGHT"
+    mode.src = "./assets/icon-sun.svg" 
 
 }
  
